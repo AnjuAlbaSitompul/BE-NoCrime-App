f2d06f95a4f139e7d358d45362e6c51e13d17736
[AnjuAlbaSitompul] <[anju@uinsu.ac.id]>
2024-08-30 07:45:46 +0700

    fix

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { logger } from "./application/logging.js";
 import { web } from "./application/web.js";
-
-web.listen(3000, `192.168.100.32`, () => {
-  logger.info("Server started on http://192.168.100.32:3000");
+const API = process.env.API_URL;
+web.listen(3000, API, () => {
+  logger.info(`Server started on http://${API}:3000`);
 });
