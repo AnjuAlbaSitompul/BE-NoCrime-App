26f14c348eb1718ca0b5ca592657744b53b1847f
[AnjuAlbaSitompul] <[anju@uinsu.ac.id]>
2024-09-09 07:12:35 +0700

    fix vercel settings

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { logger } from "./application/logging.js";
 import { web } from "./application/web.js";
-const API = process.env.API_URL;
 web.listen(3000, () => {
-  logger.info(`Server started on http://${API}:3000`);
+  logger.info(`Server started`);
 });
